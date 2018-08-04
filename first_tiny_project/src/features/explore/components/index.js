006124384ed376ff92b843e5612bfb1964fedf11
Tien Nguyen <nntien.nguyen@gmail.com>
2018-08-04 14:55:10 +0700

    code chat

--- first_tiny_project/src/features/explore/components/index.js
+++ first_tiny_project/src/features/explore/components/index.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
-import { Text, View, StatusBar, Image } from 'react-native';
+import { Text, View, StatusBar, FlatList } from 'react-native';
 
 import commonStyles from '../../../assets/styles';
 import LinearGradient from 'react-native-linear-gradient';
 
 import { CachedImage } from 'react-native-cached-image';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+
+var data = [
+  {
+    _id: 1,
+    title: 'Nhan vo cung sach se, rat dep, vo o thich lien',
+    price: '1,000,000',
+    address: '',
+    district: 'Quan 1',
+    city: 'Ho Chi Minh',
+    pictures: [
+      'https://static.chotot.com.vn/1/images/3QDrXECN27wAhCaX5sJpxd9zQ2A5aqgqxwQ9d614Hae98CQ7pQDSFGFPxkNqKeMv6E2nUKh.43ebZzzVReYLutn7S18DD1sUxigfVLh9ndiouherZuV7'
+    ]
+  },
+  {
+    _id: 2,
+    title: 'Nhan sang dep, quan 2 vo o lien',
+    price: '1,000,000',
+    address: '',
+    district: 'Quan 2',
+    city: 'Ho Chi Minh',
+    pictures: [
+      'https://static.chotot.com.vn/1/images/3QDrXECN27wANnSDUJgbu3b6MUEATPx5CDcWpb8sragqpRkeYbb7SdBRqczCrgfjdTLgQhm.EGyXKaZXG9PHtEUm77ULPwkQA3Pp5HxzRRRk9y6wz6X6'
+    ]
+  },
+  {
+    _id: 3,
+    title: 'Nha de long lanh quan 7',
+    price: '1,000,000',
+    address: '',
+    district: 'Quan 2',
+    city: 'Ho Chi Minh',
+    pictures: [
+      'https://static.chotot.com.vn/1/images/3QDrXECN27wAhCZaFa8GuLoWRAS2tuuL2GVCxUixUFj5Wu1muqx5B48JvU7MUechCjhENaB.DGjvtAyYoPTqM2ajVfTrp1c22GCe2afbhhog5JvKaecf'
+    ]
+  },
+  {
+    _id: 3,
+    title: 'Nha de long lanh quan 7',
+    price: '1,000,000',
+    address: '',
+    district: 'Quan 2',
+    city: 'Ho Chi Minh',
+    pictures: [
+      'https://static.chotot.com.vn/1/images/3QDrXECN27wAhCZaFa8GuLoWRAS2tuuL2GVCxUixUFj5Wu1muqx5B48JvU7MUechCjhENaB.DGjvtAyYoPTqM2ajVfTrp1c22GCe2afbhhog5JvKaecf'
+    ]
+  }
+];
 
 export default class Explore extends Component {
   constructor(params) {
@@ -19,10 +66,54 @@ export default class Explore extends Component {
       height: event.nativeEvent.layout.height
     });
   }
+  _keyExtractor = (item, index) => item._id;
+  _renderItem = ({ item }) => (
+    <View style={{ flex: 1, paddingRight: 20 }}>
+      <CachedImage
+        resizeMode="cover"
+        style={{
+          height: ((this.state.width - 60) / 2) * 0.7,
+          width: (this.state.width - 60) / 2,
+          borderRadius: 5
+        }}
+        source={{ uri: item.pictures[0] }}
+      />
+
+      <Text
+        style={[
+          commonStyles.defaultFont,
+          { fontSize: 15, color: '#333', marginTop: 8 }
+        ]}
+      >
+        {item.district.toUpperCase()}.{item.city.toUpperCase()}
+      </Text>
+
+      <Text
+        numberOfLines={2}
+        style={[
+          commonStyles.defaultFont,
+          { fontSize: 18, color: 'black', fontWeight: 'bold' }
+        ]}
+      >
+        {item.title}
+      </Text>
+      <Text
+        style={[
+          commonStyles.defaultFont,
+          { color: 'pink', fontSize: 18, fontWeight: 'bold' }
+        ]}
+      >
+        {item.price} VDN
+      </Text>
+      <View style={{ position: 'absolute', top: 10, right: 30 }}>
+        <Ionicons name="ios-heart" size={15} color="white" />
+      </View>
+    </View>
+  );
 
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: 'white' }}>
         <StatusBar backgroundColor="#192f6a" barStyle="light-content" />
         <View style={{ flex: 0.8 }}>
           <LinearGradient
@@ -100,10 +191,17 @@ export default class Explore extends Component {
             </View>
           </LinearGradient>
         </View>
-        <View style={{ flex: 9.2 }}>
-          <CachedImage
-            style={{ width: 20, height: 20 }}
-            source={{ uri: 'https://www.gstatic.com/webp/gallery3/1.png' }}
+        <View style={{ flex: 9.2, paddingLeft: 20 }}>
+          <FlatList
+            ItemSeparatorComponent={() => <View style={{ height: 40 }} />}
+            ListEmptyComponent={() => {
+              <View />;
+            }}
+            removeClippedSubviews={true}
+            data={data}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={this._renderItem}
+            numColumns={2}
           />
         </View>
 
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ var { db } = require('../db/index.js');
 var { users } = require('../db/user.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  users.getById('5b6083df8afaee67af85e6d5').then(u => {
-    if (u) {
-      console.log(u);
-    } else {
-      console.log('not found');
-    }
-  });
+  // users.getById('5b6083df8afaee67af85e6d5').then(u => {
+  //   if (u) {
+  //     console.log(u);
+  //   } else {
+  //     console.log('not found');
+  //   }
+  // });
 
   // users.inser().then(u => {
   //   console.log(u);
