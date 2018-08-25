0a6b60d112caf534c30e7212b8be4d8f45633a95
Tien Nguyen <nntien.nguyen@gmail.com>
2018-08-25 21:13:37 +0700

    mobile code

--- first_tiny_project/src/features/room_detail/components/index.js
+++ first_tiny_project/src/features/room_detail/components/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, StatusBar, ScrollView } from 'react-native';
-
+import { View, ScrollView } from 'react-native';
+import Swiper from 'react-native-swiper';
+import { CachedImage } from 'react-native-cached-image';
 export default class RoomDetail extends Component {
   constructor(props) {
     super(props);
@@ -17,7 +18,26 @@ export default class RoomDetail extends Component {
   render() {
     return (
       <ScrollView>
-        <View style={{ backgroundColor: 'white', flex: 1 }} />
+        <View style={{ backgroundColor: 'white', flex: 1 }}>
+          <Swiper style={{ height: 200 }}>
+            <CachedImage
+              resizeMode="cover"
+              style={{ height: 200 }}
+              source={{
+                uri:
+                  'https://static.chotot.com.vn/1/images/3QDrXECN27wAhCZaFa8GuLoWRAS2tuuL2GVCxUixUFj5Wu1muqx5B48JvU7MUechCjhENaB.DGjvtAyYoPTqM2ajVfTrp1c22GCe2afbhhog5JvKaecf'
+              }}
+            />
+            <CachedImage
+              resizeMode="cover"
+              style={{ height: 200 }}
+              source={{
+                uri:
+                  'https://static.chotot.com.vn/1/images/3QDrXECN27wAhCZaFa8GuLoWRAS2tuuL2GVCxUixUFj5Wu1muqx5B48JvU7MUechCjhENaB.DGjvtAyYoPTqM2ajVfTrp1c22GCe2afbhhog5JvKaecf'
+              }}
+            />
+          </Swiper>
+        </View>
       </ScrollView>
     );
   }
